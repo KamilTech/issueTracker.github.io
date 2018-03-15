d30ed077f235fa7a6adf1999df4c3e44285be757
Kamil <kamil@kamil.com>
2018-03-15 21:15:34 +0100

    improvement of some errors

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 "use strict";
 document.getElementById('issuesForm').addEventListener('submit', saveIssues);
 
-(function fetchIssues(data) {
+(() => {
+    fetchIssues();
+})();
+
+function fetchIssues() {
+    document.getElementById('issuesList').innerHTML = '';
+    let data = JSON.parse(localStorage.getItem('issues'));
     if (data !== null) {
         if (!(Object.keys(data).length === 0 && data.constructor === Object)) {
             let localData = data;
@@ -10,8 +16,7 @@ document.getElementById('issuesForm').addEventListener('submit', saveIssues);
             }
         } else return false;
     } else return false;
-})(JSON.parse(localStorage.getItem('issues')));
-
+};
 
 function saveIssues(e) {
     let issue = {
@@ -31,17 +36,29 @@ function saveIssues(e) {
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
     }
-    fetchIssues(JSON.parse(localStorage.getItem('issues')));
+    document.getElementById('issuesForm').reset();
+    fetchIssues();
     e.preventDefault();
 }
 
+function deleteIssue(id) {
+    let issues = JSON.parse(localStorage.getItem('issues'));
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].issuesId == id) {
+            issues.splice(i, 1);
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
 
 function createForm(id, description, severity, assignedTo, status) {
     document.getElementById('issuesList').innerHTML += '<div class="jumbotron">' +
         '<h6><small><b>Issue ID:</b> ' + id + '</small></h6>' +
         '<p><span class="badge badge-info">' + status + '</span></p>' +
         '<h3>' + description + '</h3>' +
-        '<div><p><i class="fas fa-cogs"></i> <b>' + severity + '</b></p>'+'<p><i class="fas fa-user"></i> <b>' + assignedTo + '</b></p></div>' +
+        '<div class="d-flex justify-content-start"><p><i class="fas fa-cogs"></i> <b>' + severity + '</b></p>&nbsp;&nbsp;' +
+        '<p><i class="fas fa-user"></i> <b>' + assignedTo + '</b></p></div>' +
         '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a> ' +
         '<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger">Delete</a>' +
         '</div>';
